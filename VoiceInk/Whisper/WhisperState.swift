730da38133652d22e61639390160c6f786f10aed
Mat Greten <mat.greten@companycam.com>
2025-08-18 16:39:59 -0600

    Remove license restrictions for open source build
    - Set LicenseViewModel to always return licensed state
    - Remove trial message displays from MetricsView
    - Remove trial expired messages from transcriptions
    - App now runs without any license checks

--- VoiceInk/Whisper/WhisperState.swift
+++ VoiceInk/Whisper/WhisperState.swift
@@ -345,12 +345,7 @@ class WhisperState: NSObject, ObservableObject {
                 NotificationCenter.default.post(name: .transcriptionCreated, object: newTranscription)
             }
             
-            if case .trialExpired = licenseViewModel.licenseState {
-                text = """
-                    Your trial has expired. Upgrade to VoiceInk Pro at tryvoiceink.com/buy
-                    \n\(text)
-                    """
-            }
+            // Trial expired message removed for open source build
 
             text += " "
 
